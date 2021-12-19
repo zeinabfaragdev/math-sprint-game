06c7bb9c7cb2646f4b9bfb68b0c03fc5e0d62359
Zeinab Farag <zeinabfarag454@gmail.com>
2021-12-18 19:35:46 -0500

    create equations

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const penaltyTimeEl = document.querySelector(".penalty-time");
 const playAgainBtn = document.querySelector(".play-again");
 
 // Equations
-
+let questionAmount = 0;
 let equationsArray = [];
 
 // Game Page
@@ -35,31 +35,32 @@ const wrongFormat = [];
 // Create Correct/Incorrect Random Equations
 function createEquations() {
   // Randomly choose how many correct equations there should be
-  // const correctEquations =
+  const correctEquations = Math.floor(Math.random() * questionAmount);
   // Set amount of wrong equations
-  // const wrongEquations =
+  const wrongEquations = questionAmount - correctEquations;
   // Loop through, multiply random numbers up to 9, push to array
-  // for (let i = 0; i < correctEquations; i++) {
-  //   firstNumber =
-  //   secondNumber =
-  //   const equationValue = firstNumber * secondNumber;
-  //   const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`;
-  //   equationObject = { value: equation, evaluated: 'true' };
-  //   equationsArray.push(equationObject);
-  // }
+  for (let i = 0; i < correctEquations; i++) {
+    firstNumber = Math.floor(Math.random() * 10);
+    secondNumber = Math.floor(Math.random() * 10);
+    const equationValue = firstNumber * secondNumber;
+    const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`;
+    equationObject = { value: equation, evaluated: "true" };
+    equationsArray.push(equationObject);
+  }
   // Loop through, mess with the equation results, push to array
-  // for (let i = 0; i < wrongEquations; i++) {
-  //   firstNumber =
-  //   secondNumber =
-  //   const equationValue = firstNumber * secondNumber;
-  //   wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`;
-  //   wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`;
-  //   wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`;
-  //   const formatChoice =
-  //   const equation = wrongFormat[formatChoice];
-  //   equationObject = { value: equation, evaluated: 'false' };
-  //   equationsArray.push(equationObject);
-  // }
+  for (let i = 0; i < wrongEquations; i++) {
+    firstNumber = Math.floor(Math.random() * 10);
+    secondNumber = Math.floor(Math.random() * 10);
+    const equationValue = firstNumber * secondNumber;
+    wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`;
+    wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`;
+    wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`;
+    const formatChoice = Math.floor(Math.random() * 3);
+    const equation = wrongFormat[formatChoice];
+    equationObject = { value: equation, evaluated: "false" };
+    equationsArray.push(equationObject);
+  }
+  shuffle(equationsArray);
 }
 
 // Dynamically adding correct/incorrect equations
@@ -82,3 +83,43 @@ function createEquations() {
 //   bottomSpacer.classList.add('height-500');
 //   itemContainer.appendChild(bottomSpacer);
 // }
+function countdownStart() {
+  countdown.textContent = 3;
+
+  setTimeout(() => {
+    countdown.textContent = 2;
+  }, 1000);
+  setTimeout(() => {
+    countdown.textContent = 1;
+  }, 2000);
+  setTimeout(() => {
+    countdown.textContent = "GO!";
+  }, 3000);
+}
+
+function showCountDown() {
+  splashPage.hidden = true;
+  countdownPage.hidden = false;
+  countdownStart();
+  createEquations();
+}
+
+function selectQuestionAmount(e) {
+  e.preventDefault();
+  if (questionAmount) {
+    showCountDown();
+  }
+}
+
+startForm.addEventListener("click", () => {
+  radioContainers.forEach((radio) => {
+    if (radio.children[1].checked) {
+      questionAmount = radio.children[1].value;
+      radio.classList.add("selected-label");
+    } else {
+      radio.classList.remove("selected-label");
+    }
+  });
+});
+
+startForm.addEventListener("submit", selectQuestionAmount);
